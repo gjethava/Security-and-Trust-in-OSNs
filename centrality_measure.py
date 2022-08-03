96157798dfe8f2f3dc76c93bad03ea219b2ca72e
gjethava <109677069+gjethava@users.noreply.github.com>
2022-08-03 16:58:16 +0530

    Add files via upload

---
+++ centrality_measure.py
@@ -0,0 +1,27 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random
+
+# loading soc-karate dataset 
+g1=nx.read_pajek('/content/drive/MyDrive/Colab Notebooks/soc-karate.net')
+
+# Measuring degree centrality of each node in the soc-karate network graph
+print("The degree centrality of each node in the soc-karate network graph is as follows")
+deg_cent=nx.degree_centrality(g1)
+print(deg_cent)
+
+# Measuring closeness centrality of each node in the soc-karate network graph
+print("The closeness centrality of each node in the soc-karate network graph is as follows")
+close_cent=nx.closeness_centrality(g1)
+print(close_cent)
+
+# Measuring betweenness centrality of each node in the soc-karate network graph
+print("The betweenness centrality of each node in the soc-karate network graph is as follows")
+g2=nx.karate_club_graph()
+betwn_cent=nx.betweenness_centrality(g2, k=None, normalized=True, weight=None, endpoints=False, seed=None)
+print(betwn_cent)
